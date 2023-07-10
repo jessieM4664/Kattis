b5315cd4b8d954ab59983a270e5f5956cf1b76d1
Jessica Morrison <94693340+jessieM4664@users.noreply.github.com>
2023-07-10 18:33:56 -0400

    Create fizzBuzz.java

---
+++ fizzBuzz.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class Tests {
+public static void main(String args[]) {
+  Scanner sc = new Scanner(System.in);
+  
+  int X = sc.nextInt(); // fizz
+  int Y = sc.nextInt(); // buzz
+  int N = sc.nextInt();
+  
+  for (int i = 1; i <= N; i++) {
+    if (i%X==0 && i%Y==0) {
+      System.out.println("FizzBuzz");
+    } else if (i%X==0 && i%Y!=0) {
+      System.out.println("Fizz");
+    } else if (i%X!=0 && i%Y==0) {
+      System.out.println("Buzz");
+    } else {
+      System.out.println(i);
+    }
+  }
+}}
