c9500dc4317dcff567c1845abb48e1c3796fce30
Jessica Morrison <94693340+jessieM4664@users.noreply.github.com>
2023-07-10 18:30:51 -0400

    Create Datum.java

---
+++ Datum.java
@@ -0,0 +1,121 @@
+import java.util.Scanner;
+
+class Datum {
+public static void main(String args[]) {
+  Scanner sc = new Scanner(System.in);
+  
+  int d = sc.nextInt();
+  int m = sc.nextInt();
+  
+  switch (m) {
+    case 1:
+      if (d%7==4) {System.out.print("Sunday");} else 
+      if (d%7==5) {System.out.print("Monday");} else 
+      if (d%7==6) {System.out.print("Tuesday");} else 
+      if (d%7==0) {System.out.print("Wednesday");} else 
+      if (d%7==1) {System.out.print("Thursday");} else 
+      if (d%7==2) {System.out.print("Friday");} else 
+      if (d%7==3) {System.out.print("Saturday");}
+    break;
+    case 2:
+      if (d%7==1) {System.out.print("Sunday");} else 
+      if (d%7==2) {System.out.print("Monday");} else 
+      if (d%7==3) {System.out.print("Tuesday");} else 
+      if (d%7==4) {System.out.print("Wednesday");} else 
+      if (d%7==5) {System.out.print("Thursday");} else 
+      if (d%7==6) {System.out.print("Friday");} else 
+      if (d%7==0) {System.out.print("Saturday");}
+    break;
+    case 3:
+      if (d%7==1) {System.out.print("Sunday");} else 
+      if (d%7==2) {System.out.print("Monday");} else 
+      if (d%7==3) {System.out.print("Tuesday");} else 
+      if (d%7==4) {System.out.print("Wednesday");} else 
+      if (d%7==5) {System.out.print("Thursday");} else 
+      if (d%7==6) {System.out.print("Friday");} else 
+      if (d%7==0) {System.out.print("Saturday");}
+    break;
+    case 4:
+      if (d%7==5) {System.out.print("Sunday");} else 
+      if (d%7==6) {System.out.print("Monday");} else 
+      if (d%7==0) {System.out.print("Tuesday");} else 
+      if (d%7==1) {System.out.print("Wednesday");} else 
+      if (d%7==2) {System.out.print("Thursday");} else 
+      if (d%7==3) {System.out.print("Friday");} else 
+      if (d%7==4) {System.out.print("Saturday");}
+    break;
+    case 5:
+      if (d%7==3) {System.out.print("Sunday");} else 
+      if (d%7==4) {System.out.print("Monday");} else 
+      if (d%7==5) {System.out.print("Tuesday");} else 
+      if (d%7==6) {System.out.print("Wednesday");} else 
+      if (d%7==0) {System.out.print("Thursday");} else 
+      if (d%7==1) {System.out.print("Friday");} else 
+      if (d%7==2) {System.out.print("Saturday");}
+    break;
+    case 6:
+      if (d%7==0) {System.out.print("Sunday");} else 
+      if (d%7==1) {System.out.print("Monday");} else 
+      if (d%7==2) {System.out.print("Tuesday");} else 
+      if (d%7==3) {System.out.print("Wednesday");} else 
+      if (d%7==4) {System.out.print("Thursday");} else 
+      if (d%7==5) {System.out.print("Friday");} else 
+      if (d%7==6) {System.out.print("Saturday");}
+    break;
+    case 7:
+      if (d%7==5) {System.out.print("Sunday");} else 
+      if (d%7==6) {System.out.print("Monday");} else 
+      if (d%7==0) {System.out.print("Tuesday");} else 
+      if (d%7==1) {System.out.print("Wednesday");} else 
+      if (d%7==2) {System.out.print("Thursday");} else 
+      if (d%7==3) {System.out.print("Friday");} else 
+      if (d%7==4) {System.out.print("Saturday");}
+    break;
+    case 8:
+      if (d%7==2) {System.out.print("Sunday");} else 
+      if (d%7==3) {System.out.print("Monday");} else 
+      if (d%7==4) {System.out.print("Tuesday");} else 
+      if (d%7==5) {System.out.print("Wednesday");} else 
+      if (d%7==6) {System.out.print("Thursday");} else 
+      if (d%7==0) {System.out.print("Friday");} else 
+      if (d%7==1) {System.out.print("Saturday");}
+    break;
+    case 9:
+      if (d%7==6) {System.out.print("Sunday");} else 
+      if (d%7==0) {System.out.print("Monday");} else 
+      if (d%7==1) {System.out.print("Tuesday");} else 
+      if (d%7==2) {System.out.print("Wednesday");} else 
+      if (d%7==3) {System.out.print("Thursday");} else 
+      if (d%7==4) {System.out.print("Friday");} else 
+      if (d%7==5) {System.out.print("Saturday");}
+    break;
+    case 10:
+      if (d%7==4) {System.out.print("Sunday");} else 
+      if (d%7==5) {System.out.print("Monday");} else 
+      if (d%7==6) {System.out.print("Tuesday");} else 
+      if (d%7==0) {System.out.print("Wednesday");} else 
+      if (d%7==1) {System.out.print("Thursday");} else 
+      if (d%7==2) {System.out.print("Friday");} else 
+      if (d%7==3) {System.out.print("Saturday");}
+    break;
+    case 11:
+      if (d%7==1) {System.out.print("Sunday");} else 
+      if (d%7==2) {System.out.print("Monday");} else 
+      if (d%7==3) {System.out.print("Tuesday");} else 
+      if (d%7==4) {System.out.print("Wednesday");} else 
+      if (d%7==5) {System.out.print("Thursday");} else 
+      if (d%7==6) {System.out.print("Friday");} else 
+      if (d%7==0) {System.out.print("Saturday");}
+    break;
+    case 12:
+      if (d%7==6) {System.out.print("Sunday");} else 
+      if (d%7==0) {System.out.print("Monday");} else 
+      if (d%7==1) {System.out.print("Tuesday");} else 
+      if (d%7==2) {System.out.print("Wednesday");} else 
+      if (d%7==3) {System.out.print("Thursday");} else 
+      if (d%7==4) {System.out.print("Friday");} else 
+      if (d%7==5) {System.out.print("Saturday");}
+    break;
+  }
+  
+}}
