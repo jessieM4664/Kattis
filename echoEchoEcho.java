c739c0e711b7d5c56465ee3a8edf7387f499b86c
Jessica Morrison <94693340+jessieM4664@users.noreply.github.com>
2023-07-10 18:31:34 -0400

    Create echoEchoEcho.java

---
+++ echoEchoEcho.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Tests {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        System.out.println(str + " " + str + " " + str);
+        
+        sc.close();
+    }
+}
